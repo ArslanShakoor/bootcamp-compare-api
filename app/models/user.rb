75f9ac45530ecfd282ce4c1517d5c079a13e5858
Arslan Shakoor <arslantechielive@gmail.com>
2017-09-21 03:04:02 -0400

    adding some user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
-  validates_presence_of :first_name, :last_name, :email
+  # validation of presence of fields
+  validates_presence_of :first_name, :last_name, :email, :password
+
+  validates_length_of :first_name, in: 6..26
+  validates_length_of :last_name, in: 6..26
+  validates_length_of :email, in: 10..100
+  validates_length_of :password, in: 6..50
+
 	acts_as_token_authenticatable
 
 	# Include default devise modules. Others available are:
@@ -14,5 +21,5 @@ class User < ApplicationRecord
   end
 
   has_many :camps
- 
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-   it{ should validate_presence_of(:first_name) }
-   it{ should validate_presence_of(:last_name) }
-   
+
+  # validation of  fields presence
+  it{ should validate_presence_of(:first_name) }
+  it{ should validate_presence_of(:last_name) }
+  it{ should validate_presence_of(:email) }
+  it{ should validate_presence_of(:password) }
+
+  # validation of fields of lengths
+  it {should validate_length_of(:first_name).
+    is_at_least(6).is_at_most(26)}
+  it {should validate_length_of(:last_name).
+    is_at_least(6).is_at_most(26)}
+  it {should validate_length_of(:email).
+    is_at_least(10).is_at_most(100)}
+  it {should validate_length_of(:password).
+    is_at_least(6).is_at_most(50)}
+
+
 end
