18c3960e8367df862c1265996d224e25f3518770
Arslan Shakoor <arslantechielive@gmail.com>
2017-10-03 17:00:08 -0400

    add the permission of put and delete in cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
 
 acts_as_token_authentication_handler_for User, fallback: :none
- 
+
 include Response
 include ExceptionHandler
 
--- config/application.rb
+++ config/application.rb
@@ -14,23 +14,24 @@ require "action_cable/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
- 
 
- 
+
+
 
 Bundler.require(*Rails.groups)
 
 module BootcampCompare
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-     
-    
+
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete, :put]
       end
     end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -38,8 +39,8 @@ module BootcampCompare
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-     
+
     config.api_only = true
-    
+
   end
 end
